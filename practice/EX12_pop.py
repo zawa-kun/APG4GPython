fa775361d10fd7480ed5d53aee2409d1ad12d54b
zawa-kun <shult.0219@gmail.com>
2025-01-24 17:09:40 +0900

    EX12:pop version

---
+++ practice/EX12_pop.py
@@ -0,0 +1,34 @@
+S = input().strip()
+
+result = 1
+calculation = list(S[1::2])
+print(calculation)
+for i in range(len(calculation)):
+    x = calculation.pop(0)
+    if x == "+":
+        result += 1
+    elif x == "-":
+        result -= 1
+
+print(result)
+
+"""
+なぜ下記のロジックだと動かないのか？
+
+原因: 
+for x in calculation:
+    処理
+    calculation.pop(0)
+
+for文のループが元のリストの長さで動くのに対し、pop()でリストが短くなるため、
+一部の要素がスキップされるから。
+"""
+
+# for x in calculation:
+#     if x == "+":
+#         result += 1
+#     elif x == "-":
+#         result -= 1
+#     calculation.pop(0)
+
+# print(result)
